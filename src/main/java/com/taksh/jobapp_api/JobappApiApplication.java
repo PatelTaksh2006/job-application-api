052d3bbd34f4b022b78cf3eb63a42db4b197ae5d
PatelTaksh2006 <takshk2006@gmail.com>
2025-03-23 19:29:42 +0530

    Project setup

---
+++ src/main/java/com/taksh/jobapp_api/JobappApiApplication.java
@@ -0,0 +1,13 @@
+package com.taksh.jobapp_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobappApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobappApiApplication.class, args);
+	}
+
+}
